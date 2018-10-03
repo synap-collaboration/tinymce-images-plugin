490f89cec1afc0a6b4bf55c3ed4e9b234088e6b3
Jesse Cooper <jesseandcooper@gmail.com>
2018-10-03 16:35:29 -0500

    Fill out file reading and img insertion logic

--- src/plugin.js
+++ src/plugin.js
@@ -1,42 +1,70 @@
-tinymce.create('tinymce.plugins.SynapImage', {
+function initPlugin() {
+  const onFileInput = function(editor, file) {
+    const img = editor.dom.create('img');
+    editor.selection.setNode(img);
+    // img.setAttribute('data-mce-id', '__mcenew');
 
-  init: function (ed) {
-      ed.addButton('upload-button', {
-          icon: 'image',
-          tooltip: 'Insert an image',
-          onClick: (wrappingEvent) => {
-            const input = document.createElement('input');
+    // editor.focus();
+    // editor.selection.setContent(img.outerHTML);
 
-            input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
+    // const insertedElem = editor.dom.select('*[data-mce-id="__mcenew"]')[0];
+    // insertedElem.setAttribute('data-mce-id', null);
 
-            input.onchange = (e) => {
-              e.preventDefault();
-              const files = e.target.files;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const base64Data = reader.result;
+      img.src = base64Data;
 
-              e.target.value = !files.length ? null : files[0];
-
-              if (files.length) {
-                wrappingEvent.target.onchange();
-              }
-            }
-
-            // input.click();
-          },
-          onChange: (e) => {
-            const file = e.control.value();
-            console.dir(file);
-          }
-      });
-  },
-
-  getInfo: function () {
-      return {
-          longname: 'synap-image',
-          author: 'Jesse Cooper',
-          version: tinymce.majorVersion + '.' + tinymce.minorVersion
-      };
+      const handler = editor.getParam('images_upload_handler');
+      handler(
+        file,
+        (url) => {
+          img.src = url;
+        },
+        (err) => {
+          alert('Upload failed');
+        }
+      );
+    }
   }
-});
 
-tinymce.PluginManager.add('synap-image', tinymce.plugins.SynapImage);
+  tinymce.create('tinymce.plugins.SynapImages', {
+
+    init: function (ed) {
+        ed.addButton('upload-button', {
+            icon: 'image',
+            tooltip: 'Insert an image',
+            onClick: function (wrappingEvent) {
+              const input = document.createElement('input');
+
+              input.setAttribute('type', 'file');
+              input.setAttribute('accept', 'image/*');
+
+              input.onchange = (e) => {
+                e.preventDefault();
+                const files = e.target.files;
+
+                if (files.length) {
+                  onFileInput(ed, files[0]);
+                }
+              }
+
+              input.click();
+            }
+        });
+    },
+
+    getInfo: function () {
+        return {
+            longname: 'synap-images',
+            author: 'Jesse Cooper',
+            version: tinymce.majorVersion + '.' + tinymce.minorVersion
+        };
+    }
+  });
+
+  tinymce.PluginManager.add('synap-images', tinymce.plugins.SynapImages);
+};
+
+initPlugin();
